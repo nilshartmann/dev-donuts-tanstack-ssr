63b3fb0995b4e8c5b446c7622f28cf6a05d79066
Nils Hartmann <nils@nilshartmann.net>
2025-05-22 16:10:56 +0200

    03: SSR funktioniert leider nur mit JavaScript (javascript im Browser ausschalten)

--- tanstack2025/src/demo-config.ts
+++ tanstack2025/src/demo-config.ts
@@ -4,4 +4,6 @@ export const showQueryDevTools = false;
 // Das würde man in einer echten Anwendung so nicht machen,
 //  hier nur für die Demo, um den Umstieg auf SSR
 //  zu zeigen
-export const enableSsr = false;
+// -> im ersten Schritt funktioniert das SSR der GANZEN Seite
+//    nur mit JavaScript...
+export const enableSsr = true;
