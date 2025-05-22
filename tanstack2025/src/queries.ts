4cc7dd46d26c2023dcbad86195dac48097eb5c69
Nils Hartmann <nils@nilshartmann.net>
2025-05-22 16:27:23 +0200

    09: Donuts per Server Function auf dem Server laden

--- tanstack2025/src/queries.ts
+++ tanstack2025/src/queries.ts
@@ -9,18 +9,38 @@ const ky = _ky.extend({
   timeout: 30000,
 });
 
+const loadDonut = createServerFn({
+  method: "POST",
+})
+  .validator((data) => {
+    if (typeof data === "string") {
+      return data;
+    }
+
+    throw new Error("invalid data :-(");
+  })
+  .handler(async ({ data: donutId }) => {
+    console.log("LOADING DONUT ON SERVER WITH ID", donutId);
+    const r = await ky
+      // "Eigentlich" müssten jetzt auf Donut + Kommentare insg.
+      // 1200ms gewartet werden (s. slow unten)
+      // es dauert aber länger, weil beide Request NACHEINANDER
+      // ausgeführt werden
+      .get(`http://localhost:7200/api/donuts/${donutId}?slow=125`)
+      .json();
+    return DonutDto.parse(r);
+  });
+
 export const fetchDonutDetailOpts = (donutId: string) =>
   queryOptions({
     queryKey: ["donuts", "detail", donutId],
     async queryFn() {
-      const r = await ky
-        // "Eigentlich" müssten jetzt auf Donut + Kommentare insg.
-        // 1200ms gewartet werden (s. slow unten)
-        // es dauert aber länger, weil beide Request NACHEINANDER
-        // ausgeführt werden
-        .get(`http://localhost:7200/api/donuts/${donutId}?slow=1200`)
-        .json();
-      return DonutDto.parse(r);
+      // Wenn wir jetzt durhc die App navigieren,
+      // werden die Donuts auf dem Server geladen 😋
+
+      return loadDonut({
+        data: donutId,
+      });
     },
   });
 
